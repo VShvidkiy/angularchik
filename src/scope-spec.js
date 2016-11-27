dbb68b4f6e853a77fa2732548ef05a97e1ec3c36
Platon Korzh <platon@korzh.io>
2016-11-27 12:40:01 +0200

    scope-spec: $watch && $digest

--- src/scope-spec.js
+++ src/scope-spec.js
@@ -8,4 +8,24 @@ describe('Scope', function() {
 
 		expect(scope.a).toBe(10);
 	});
+
+	/*describe('$digest', function() {
+		var scope = null;
+
+		beforeEach(function() {
+			scope = new Scope();
+		});
+
+		it('calls watch callback function on $digest', function() {
+			var watchCallback = jasmine.createSpy();
+
+			scope.$watch(function() {
+				return 'name';
+			}, watchCallback);
+
+			scope.$digest();
+
+			expect(watchCallback).toHaveBeenCalled();
+		});
+	});*/
 });
